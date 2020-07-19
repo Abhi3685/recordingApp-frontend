e176aa6eddbcde5c1f3b264e2a59355ec7090043
Abhishek Bindra <abhishek.bindra.ab@gmail.com>
2020-07-19 22:53:29 +0530

    Progress

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -11,7 +11,7 @@ import { Player, BigPlayButton, LoadingSpinner, ControlBar } from 'video-react';
 
 let recorder, streams, blob, myScreen, myAudio;
 
-function uploadToCloudinary(history, setIsUploading) {
+function uploadToCloudinary(history) {
     var msg_element = document.querySelector(".upload_msg");
     msg_element.classList.remove("invisible");
     var file = new File([blob], "recording.mp4");
@@ -29,7 +29,7 @@ function uploadToCloudinary(history, setIsUploading) {
                 name: 'Recording-' + Date.now(),
                 duration: res.data.duration,
                 createdAt: res.data.created_at,
-                url: videoUrl.substr(0, videoUrl.length - 3) + 'mp4',
+                url: videoUrl.substr(0, videoUrl.length - 3) + 'mkv',
                 thumb: res.data.secure_url.substr(0, res.data.secure_url.length - 3) + 'jpg',
                 publicId: res.data.public_id
             };
@@ -48,7 +48,6 @@ function Recorder() {
     const [config] = useState({ mode: location.state.mode, audio: location.state.isAudioEnabled });
     const [isRecording, setIsRecording] = useState(0);
     const [url, setUrl] = useState("");
-    const [isUploading, setIsUploading] = useState(false);
 
     useEffect(() => {
         if (!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
@@ -74,7 +73,7 @@ function Recorder() {
         });
     }
 
-    function stopCallback() {
+    function stopCallback(redirect) {
         recorder.stopRecording(function () {
             if (config.mode === "Screen + Cam" && config.audio === 1) {
                 streams.forEach(function (stream) {
@@ -99,6 +98,8 @@ function Recorder() {
                     track.stop();
                 });
             }
+
+            if (redirect) history.push("/dashboard");
 
             getSeekableBlob(recorder.getBlob(), function (seekableBlob) {
                 blob = seekableBlob;
@@ -260,7 +261,7 @@ function Recorder() {
 
                 <div className="flex">
                     <button onClick={stopCallback} className="transition duration-300 ease-in focus:outline-none hover:bg-yellow-600 w-64 bg-yellow-500 px-5 py-3 rounded-lg font-montserratSemiBold ml-16">Stop Recording</button>
-                    <button onClick={() => history.push("/dashboard")} className="transition duration-300 ease-in focus:outline-none hover:bg-red-600 w-64 bg-red-500 px-5 py-3 rounded-lg font-montserratSemiBold ml-16">Cancel Recording</button>
+                    <button onClick={() => stopCallback('redirect')} className="transition duration-300 ease-in focus:outline-none hover:bg-red-600 w-64 bg-red-500 px-5 py-3 rounded-lg font-montserratSemiBold ml-16">Cancel Recording</button>
                 </div>
             </div>
 
@@ -284,7 +285,7 @@ function Recorder() {
                         </Player>
                     </div>
                     <div className="flex justify-center">
-                        <button className="transition duration-200 ease-in focus:outline-none hover:bg-green-500 bg-green-600 shadow-md text-white px-8 py-2 rounded w-56 mr-8" onClick={() => uploadToCloudinary(history, setIsUploading)}>Save to cloud</button>
+                        <button className="transition duration-200 ease-in focus:outline-none hover:bg-green-500 bg-green-600 shadow-md text-white px-8 py-2 rounded w-56 mr-8" onClick={() => uploadToCloudinary(history)}>Save to cloud</button>
                         <button className="transition duration-200 ease-in focus:outline-none hover:bg-red-500 bg-red-600 shadow-md text-white px-8 py-2 rounded w-56" onClick={() => history.push("/dashboard")}>Destroy recording</button>
                     </div>
                     <p className="upload_msg invisible font-montserratSemiBold text-lg pt-5 text-center">Uploading ... Please Wait ....</p>
